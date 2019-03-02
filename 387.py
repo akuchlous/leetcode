785b0ee6e66a04575af06bc166465e3c84649b41
Ankur Kuchlous <akuchlou@akamai.com>
2019-03-02 10:53:25 -0800

    387.py

--- 387.py
+++ 387.py
@@ -13,14 +13,11 @@ class Solution(object):
             else:
                 uniq[s[i]] += 1 
                 
-        i = 0 
-	print (uniq)
-        for k in uniq.keys():
-            if (uniq[k] == 1 ): return i
-            i+=uniq[k]
-            
-        return -1
+        for i in range(len(s)):
+            if (uniq[s[i]] == 1 ): return i
 
+	return -1
 
 print (Solution().firstUniqChar("dddccdbba"))
+print (Solution().firstUniqChar("loveleetcode"))
 
